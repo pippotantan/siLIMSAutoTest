19ea5f4e0cbcd1dfe28a628a2b4624c97c8cad7d
pippotantan <pippo.tantan@gmail.com>
2023-06-22 14:26:46 +0800

    updated cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -5,16 +5,10 @@ module.exports = defineConfig({
   video: false,
   record: false,
   projectId: 'c9gjbc',
-  env: {
-    api_url: 'http://silims2.sitrovainnovation.com/client/#/login',
-    client_url: 'http://silims2.sitrovainnovation.com',
-  },
+  
   defaultCommandTimeout: 10000,
   e2e: {
     baseUrl:'http://silims.sitrovainnovation.com',
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
     specPattern: 'cypress/e2e/login-user.spec.js',
     testIsolation: false
   }
